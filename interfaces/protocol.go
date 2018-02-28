ce68f3c93fd0abf0d28b64b7b3fa67def88bb253
Feng Yifei <fengyfei@gmail.com>
2018-02-28 15:04:49 +0800

    protocol wrapper

--- interfaces/protocol.go
+++ interfaces/protocol.go
@@ -13,3 +13,11 @@ import (
 type Protocol interface {
 	Handler(conn net.Conn, close <-chan struct{})
 }
+
+// ProtocolHandler wraps a common function as a Protocol.
+type ProtocolHandler func(conn net.Conn, close <-chan struct{})
+
+// Handler is a Protocal implementation.
+func (f ProtocolHandler) Handler(conn net.Conn, close <-chan struct{}) {
+	f(conn, close)
+}
